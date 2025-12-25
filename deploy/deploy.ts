d6431c8e5effd59a61a84eb079b72d925341e121
sarahhughes0x <yuebpfza270@outlook.com>
2025-12-25 16:05:42 +0800

    Update deploy script for ShadowMint

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedShadowMint = await deploy("ShadowMint", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  await hre.deployments.save("ShadowMint", deployedShadowMint);
+  console.log(`ShadowMint contract: `, deployedShadowMint.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_shadowMint"; // id required to prevent reexecution
+func.tags = ["ShadowMint"];
